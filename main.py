9f19522499b2f830bcc12f221c0291646fa180ab
SakthiAi-Engineer <sakthiselvamani17@gmail.com>
2025-08-21 22:01:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ import json
 SUPABASE_URL = str(st.secrets["SUPABASE_URL"])
 SUPABASE_KEY = str(st.secrets["SUPABASE_KEY"])
 DATABASE_URL = str(st.secrets["DATABASE_URL"])
-print("Database URL:", DATABASE_URL)
 
 # NVIDIA API configuration
 NVIDIA_API_KEY = str(st.secrets["NVIDIA_API_KEY"])
